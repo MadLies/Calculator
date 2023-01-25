ff5413e635d3e07e5d1a77cb4d8dbc30dc3c7b7d
MadLies <jwilchesc@unal.edu.co>
2023-01-25 00:02:06 -0500

    Commands

---
+++ Calculator/Calculator/MVVM/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,34 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator.MVVM.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class CalculatorViewModel
+    {
+        public string Formula { get; set; }
+        public string Result { get; set; } = "0";
+
+        public ICommand OperationCommand =>
+            new Command((number) => { Formula += number; });
+
+        public ICommand ResetCommand => new Command(() => {
+            Result= "0";
+            Formula= ""; });
+
+        public ICommand DeleteCommand => new Command(() =>
+        {
+            if (Formula.Length > 0)
+            {
+                Formula = Formula.Substring(0, Formula.Length - 1);
+            }
+        });
+
+    }
+        
+}
--- Calculator/Calculator/MVVM/Views/CalculatorView.xaml.cs
+++ Calculator/Calculator/MVVM/Views/CalculatorView.xaml.cs
@@ -1,3 +1,5 @@
+using Calculator.MVVM.ViewModels;
+
 namespace Calculator.MVVM.Views;
 
 public partial class CalculatorView : ContentPage
@@ -5,5 +7,6 @@ public partial class CalculatorView : ContentPage
 	public CalculatorView()
 	{
 		InitializeComponent();
+		BindingContext = new CalculatorViewModel();
 	}
 }

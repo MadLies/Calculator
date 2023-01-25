bc79974805cc0c51da42ebb7f26189773cd55a95
MadLies <jwilchesc@unal.edu.co>
2023-01-24 21:50:37 -0500

    Grig Creation

---
+++ Calculator/Calculator/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Calculator.MVVM.Views;
+
+namespace Calculator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new CalculatorView();
+	}
+}
---
+++ Calculator/Calculator/MVVM/Views/CalculatorView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Calculator.MVVM.Views;
+
+public partial class CalculatorView : ContentPage
+{
+	public CalculatorView()
+	{
+		InitializeComponent();
+	}
+}
